58a0258677e8eb428b4917ca2f0c30f49b591e4e
Florian Frank <flori@ping.de>
2019-06-27 12:18:57 +0200

    Logger has still to be configured manually

--- lib/betterlog/railtie.rb
+++ lib/betterlog/railtie.rb
@@ -1,7 +1,6 @@
 module Betterlog
   class Railtie < Rails::Railtie
     initializer "betterlog_railtie.configure_rails_initialization" do
-      Rails.logger = Betterlog::Logger.new(Redis.new) # TODO
       Rails.logger.formatter = Betterlog::LogEventFormatter.new
     end
   end
