ab29220721e2b7e5f326155a0f2f09eb7514a23f
Florian Frank <flori@ping.de>
2019-11-06 16:59:41 +0100

    Avoid problem where honeybadger modified its
    notifier argument to be circular.

--- lib/betterlog/version.rb
+++ lib/betterlog/version.rb
@@ -1,6 +1,6 @@
 module Betterlog
   # Betterlog version
-  VERSION         = '0.7.1'
+  VERSION         = '0.7.2'
   VERSION_ARRAY   = VERSION.split('.').map(&:to_i) # :nodoc:
   VERSION_MAJOR   = VERSION_ARRAY[0] # :nodoc:
   VERSION_MINOR   = VERSION_ARRAY[1] # :nodoc:
--- spec/betterlog/log/event_spec.rb
+++ spec/betterlog/log/event_spec.rb
@@ -1,22 +1,29 @@
 require 'spec_helper'
 
 describe Betterlog::Log::Event do
+  describe '#as_hash' do
+    it 'dups hash before returning it' do
+      event = described_class.new
+      expect(event.as_hash).not_to equal event.instance_variable_get(:@data)
+    end
+  end
+
   describe '#to_json' do
     it 'can deal with circular arrays' do
       circular_array = [].tap { |arr| arr << arr }
-      event = Betterlog::Log::Event.new(array: circular_array)
+      event = described_class.new(array: circular_array)
       expect(event.to_json).to include('"array":["circular"]')
     end
 
     it 'can deal with circular hashes' do
       circular_hash = {}.tap { |hash| hash['foo'] = hash }
-      event = Betterlog::Log::Event.new(hash: circular_hash)
+      event = described_class.new(hash: circular_hash)
       expect(event.to_json).to include('"hash":{"foo":"circular"}')
     end
 
     it 'does not replace repeated scalar objects with "circular"' do
       ary = [true, false, nil, 23, 3.14]
-      event = Betterlog::Log::Event.new(ary: ary << ary.dup)
+      event = described_class.new(ary: ary << ary.dup)
       expect(event.to_json).to include(<<~end.strip)
         "ary":[true,false,null,23,3.14,[true,false,null,23,3.14]]
       end
